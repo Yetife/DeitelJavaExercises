6f9cac6825f7e60c0fe5c4948d3b7d9eb86eee25
Yetife <tifetreats21@gmail.com>
2021-10-08 13:52:22 +0100

    added files

--- src/schoolOOP/Employee.java
+++ src/schoolOOP/Employee.java
@@ -1,2 +1,21 @@
-package schoolOOP;public class Employee {
+package schoolOOP;
+
+//import chapterThree.Date;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Employee extends Person{
+    private String office;
+    private BigDecimal Salary;
+    private Date dateHired;
+
+    public void dateHired(Date datehired)
+    {
+        this.dateHired = datehired;
+    }
+    public Date getDateHired() {
+        return dateHired;
+    }
+
 }
