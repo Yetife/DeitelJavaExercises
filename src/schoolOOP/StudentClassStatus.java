e543e1e740ebda6739801b494704a0f67cd67d88
Yetife <tifetreats21@gmail.com>
2021-10-08 13:53:21 +0100

    added files

--- src/schoolOOP/StudentClassStatus.java
+++ src/schoolOOP/StudentClassStatus.java
@@ -1,2 +1,10 @@
-package schoolOOP;public enum StudentClassStatus {
+package schoolOOP;
+
+public enum StudentClassStatus {
+    FRESHMAN, SOPHOMORE, JUNIOR, SENIOR;
+
+    @Override
+    public String toString() {
+        return "StudentClassStatus{}";
+    }
 }
